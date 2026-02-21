ad80c3885f2cf1f02b44afcc0c46f4dfbb1baeea
riquezajoven12-beep <riqueza.joven.12@gmail.com>
2026-02-21 15:26:29 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2020',
-    minify: 'terser',
-    sourcemap: false,
-  },
 })
